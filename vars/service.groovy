6553049f6af179eb8d7f427884fad30cccc06f32
Dusty Burwell <dustyburwell@gmail.com>
2017-07-30 21:15:01 -0500

    Update service.groovy

--- vars/service.groovy
+++ vars/service.groovy
@@ -1,25 +1,34 @@
-node {
-    stage("Build") {
-        print("Building")
-    }
-    
-    stage("Test") {
-        print("Testing")
-    }
-    
-    stage("Deploy Canary") {
-        print("Deploying Canary")
-    }
-    
-    stage("Test Canary") {
-        print("Testing Canary")
-    }
-    
-    stage("Human Testing") {
-        input(message: 'Manual testing succeeded?', ok: 'Yes')
-    }
-    
-    stage("Deploy Production") {
-        print("Deploying Production")
+def call(body) {
+    // evaluate the body block, and collect configuration into the object
+    def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
+
+    // now build, based on the configuration provided
+    node {
+        stage("Build") {
+            print("Building")
+        }
+
+        stage("Test") {
+            print("Testing")
+        }
+
+        stage("Deploy Canary") {
+            print("Deploying Canary")
+        }
+
+        stage("Test Canary") {
+            print("Testing Canary")
+        }
+
+        stage("Human Testing") {
+            input(message: 'Manual testing succeeded?', ok: 'Yes')
+        }
+
+        stage("Deploy Production") {
+            print("Deploying Production")
+        }
     }
 }
